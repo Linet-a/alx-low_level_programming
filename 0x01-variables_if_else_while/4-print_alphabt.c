7264914a9ee7abbd88d5e82b97d1450cea9f6f13
Linet Okinyi <linetawino2018@gmail.com>
2023-02-18 05:43:26 +0300

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabet in lowercase foloed by a new line.
+ *except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
