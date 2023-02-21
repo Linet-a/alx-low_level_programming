1fe62136ebfcc68180bde08fc3b05d288e67bd40
Linet Okinyi <linetawino2018@gmail.com>
2023-02-22 01:27:08 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the ansolute value of an integer
+ * @n: The number to be computed
+ * Return: 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return n;
+
+}
