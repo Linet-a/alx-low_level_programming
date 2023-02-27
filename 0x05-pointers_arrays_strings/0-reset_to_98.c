ec0303be19ea55dfe01742af336e21e33b96fbb6
Linet Okinyi <linetawino2018@gmail.com>
2023-02-27 20:07:00 +0300

    a function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as a param and update value to 98
+ * @n: a pointer to the parameter we want to set to 402
+ * Return: Always 0 (sucess)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
