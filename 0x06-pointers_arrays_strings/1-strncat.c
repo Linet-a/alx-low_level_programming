09696d2c0ea2f2dee75d1985e5e7df9c27314ad4
Linet Okinyi <linetawino2018@gmail.com>
2023-03-02 13:07:00 +0300

    strncat- cocats two strings at n bytes from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,14 +13,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	
+
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while (j < n && j != '\0')
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
