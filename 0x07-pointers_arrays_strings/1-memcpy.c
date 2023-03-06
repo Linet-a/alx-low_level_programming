1b98def6480ad4ebab097bf59e93a29b854c20e4
Linet Okinyi <linetawino2018@gmail.com>
2023-03-06 17:29:01 +0300

    function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/**
+ * *_memcpy - function definition
+ * @dest: the destination address
+ * @src: the source address
+ * @n: number of times to copy
+ * Description: copies the src to dest n times
+ * Return: copied string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+	return (dest);
+}
