ee39bae42b8387333bdcd2b137533932743e9744
Linet Okinyi <linetawino2018@gmail.com>
2023-03-07 01:48:39 +0300

    sets the value of a ptr to a char

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,32 +1,24 @@
-#include <stdio.h>
+
+
 #include "main.h"
 
 /**
- * print_diagsums - function declaration
- * @a: the array
- * @size: diagonal size
+ * set_string - function declaration
+ * @s: the pointer
+ * @to: the char
  * Return: void
  */
 
-void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 /**
- * print_diagsums - function definition
- * @a: the array to be used
- * @size: the size of the diagonal
- * Return: void
+ * set_string - function definition
+ * @s: the pointer to use
+ * @to: the string value
+ * Description: set the value of a pointer to a char
  */
 
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int index;
-	int sum_one = 0;
-	int sum_two = 0;
-
-	for (index = 0; index < size; index++)
-	{
-		sum_one += a[(index * size) + index];
-		sum_two += a[(size - 1) + ((size - 1) * index)];
-	}
-	printf("%d, %d\n", sum_one, sum_two);
+	*s = to;
 }
