e9d61ec3ba48345a28f009675fa4e7cd7e69144b
Linet Okinyi <linetawino2018@gmail.com>
2023-03-08 19:47:21 +0300

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int _pow_recursion(int x, int y);
+
+/**
+ * _pow_recursion - function definition
+ * @x: the integer to power
+ * @y: the power to raise to
+ * Description: raise x to power y
+ * Return: an integer value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+	{
+		return (1);
+	}
+}
