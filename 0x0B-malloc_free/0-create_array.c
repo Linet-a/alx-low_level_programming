993d7560421d6948e2db4ff0c79eb24f2fb6a382
Linet Okinyi <linetawino2018@gmail.com>
2023-03-13 16:49:20 +0300

    debbuging 0-create_array.c file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,17 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - function declaration
+ * @size: size of array
+ * @c: chararacter to initialize array
+ * Return: a char value
+ */
+
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    
-    char* arr = (char*) malloc (sizeof(char) * size);
+	unsigned int i;
+	char *arr;
 
-    if (size == 0)
-        {
-            return (NULL);
-        }
-    
-    for (i = 0; i < size; i++)
-    {
-        arr[i] = c;
-    }
-    return arr;
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+	arr = (char *)malloc(sizeof(char) * size);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
 }
