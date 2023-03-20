8d2edac3870187a300c748b9e78156caf9297503
Linet Okinyi <linetawino2018@gmail.com>
2023-03-20 20:57:15 +0300

    created 0-object_like_macro.h file

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_lIKE_MACRO_H
+
+/**
+ * File: 0-object-like_macro.h
+ * Description: It defies a macro named size of size 1024
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+0x0D. C - Preprocessor
+
+This is alx task project on C - Preprocessor
