9b14ea60e9fe2e52d52bbca1744704708140b469
Linet Okinyi <linetawino2018@gmail.com>
2023-05-05 01:14:40 +0300

    update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,20 +1,46 @@
-#include "main.h"
+include "main.h"
 /**
- * print_binary - prints binary representation of a num
+ * _power - calculate base and pow
+ * @base: base
+ * @pow: power
+ * Return: value of base and power
+ */
+unsigned long int _power(unsigned int base, unsigned int pow)
+{
+	unsigned long int number;
+	unsigned int k;
+
+	n = 1;
+	for (k = 1; k <= pow; k++)
+		number *= base;
+	return (number);
+}
+/**
+ * print_binary - prints the binary representation of a number
  * @n: the unsigned long int to be printed
  * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
+	unsigned long int div, res;
+	char flg;
+
+	flg = 0;
+	div = _power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (div != 0)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0);
-	}
-	else
-	{
-		-putchar('0');
+		res = n & div;
+		if (res == div)
+		{
+			flg = 1;
+			_putchar('1');
+
+		}
+		else if (flg == 1 || div == 1)
+		{
+			_putchar('0');
+		}
+		div >>= 1;
 	}
 }
